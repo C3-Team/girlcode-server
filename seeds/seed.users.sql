5cfbdc3cf643bd3e81cef02758702e8c03d8745a
Nika Darab <nikadarab@pm.me>
2021-01-21 18:44:24 -0600

    seeding

---
+++ seeds/seed.users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_name, user_email, user_password)
+VALUES
+  ('Nika Darab', 'nikadarab@pm.me','abcdef'),
+  ('Xavier Duncan','xmd404@pm.me','ddsjdd'),
+  ('Pegah Darab','pegahdarab@gmail.com','kjdjfdf')
--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
@@ -16,7 +15,7 @@ app.use(helmet());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, world!!!!");
 });
 
 app.use(function errorHandler(error, req, res, next) {
