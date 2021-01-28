eb61217932701a1986d958c46109a0848c3a1bff
Nika Darab <nikadarab@pm.me>
2021-01-28 17:16:31 -0600

    config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DB_URL || "postgresql://nikadarab@localhost/girlcode",
+    process.env.DATABASE_URL || "postgresql://nikadarab@localhost/girlcode",
   API_TOKEN: process.env.API_TOKEN || "9bde2e85-c6e2-4118-aaa5-e4a8b186a881",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
