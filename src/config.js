6c2a27ccd1304405946d328bbe76487c7fcd2020
Nika Darab <nikadarab@pm.me>
2021-01-28 15:14:03 -0600

    added error handler and some other changes

--- src/config.js
+++ src/config.js
@@ -2,5 +2,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://nikadarab@localhost/girlcode",
+  DATABASE_URL:
+    process.env.DB_URL || "postgresql://nikadarab@localhost/girlcode",
+  API_TOKEN: process.env.API_TOKEN || "9bde2e85-c6e2-4118-aaa5-e4a8b186a881",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://nikadarab@localhost/girlcode-test",
 };
