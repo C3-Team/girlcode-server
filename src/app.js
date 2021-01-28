348d5f70f37f75c3431c9126a47bae06e2b0c8c4
Nika Darab <nikadarab@pm.me>
2021-01-28 15:18:06 -0600

    validate bearer token

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const errorHandler = require("./errorHandler");
 const { NODE_ENV } = require("./config");
+const validateBearerToken = require("./validateBearerToken");
+
 const app = express();
 const inventoriesRouter = require("./inventories/inventories-router");
 const needsRouter = require("./needs/needs-router");
@@ -14,22 +17,12 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-
+app.use(validateBearerToken);
 app.use("/api/inventories", inventoriesRouter);
 app.use("/api/needs", needsRouter);
 app.get("/", (req, res) => {
   res.send("Hello, world!!!");
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
-
+app.use(errorHandler);
 module.exports = app;
